709c257c55d5c45f5fd22d024e0ad320f95bdb41
JesseKinsella1 <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 14:38:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+day = 7  # integer, no decimal
+print(day)
+
+temp = -15
+print(temp)
+
+weight = 165.8 # float's have decimals
+print(weight)
+
+print(3+6) # do some math
+
+print(day+3) # day from 3 days from now
+
+print(weight*2)
+
+print(weight/2)
+
+print(weight-15)
+
+number_of_legs = 4
+cost_of_pizza = 16.97
+
+print(number_of_legs + 3)
